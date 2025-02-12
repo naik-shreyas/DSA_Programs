7a9d8d80aba727606227a455b35eca428095e93d
naikshrey <naik.shrey@gmail.com>
2025-02-12 22:38:52 +0530

    DSA - Doubly Linked List changes

--- src/ds/doublylinkedlist/DoublyLinkedList.java
+++ src/ds/doublylinkedlist/DoublyLinkedList.java
@@ -1,4 +1,25 @@
 package ds.doublylinkedlist;
 
 public class DoublyLinkedList {
+    private Node first;
+    private Node last;
+
+    public DoublyLinkedList()
+    {
+        this.first=null;
+        this.last=null;
+    }
+
+    public boolean isEmpty()
+    {
+        return (first==null);
+    }
+
+    public void insertFirst(int data)
+    {
+        Node newNode = new Node();
+        newNode.data = data;
+
+
+    }
 }
