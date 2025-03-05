a63907077ea5bd4e1299fb555e831e8880307549
naikshrey <naik.shrey@gmail.com>
2025-03-05 23:48:24 +0530

    DSA - Selection Sort

---
+++ src/algos/selectionsort/App.java
@@ -0,0 +1,12 @@
+package algos.selectionsort;
+
+public class App {
+    public static void main(String[] args) {
+        int[] myArray = selectionSort(new int[] {23, 56 ,7 ,98});
+        System.out.println(myArray);
+    }
+
+    public static int[] selectionSort(int[] a) {
+    return a;
+    }
+}
