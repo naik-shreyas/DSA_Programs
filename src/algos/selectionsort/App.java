c72e1aff5b40766ac05f71d79d4071ab7c9ef05a
naikshrey <naik.shrey@gmail.com>
2025-03-12 00:33:55 +0530

    DSA - Selection Sort

--- src/algos/selectionsort/App.java
+++ src/algos/selectionsort/App.java
@@ -2,11 +2,25 @@ package algos.selectionsort;
 
 public class App {
     public static void main(String[] args) {
-        int[] myArray = selectionSort(new int[] {23, 56 ,7 ,98});
-        System.out.println(myArray);
+        int[] myArray = selectionSort(new int[]{23, 56, 7, 98});
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
     }
 
     public static int[] selectionSort(int[] a) {
-    return a;
+        for (int i = 0; i < a.length; i++) {
+            int minimum = i;
+            for (int j = 1; j < a.length; j++) {
+                if (a[j] < a[i]) {
+                    minimum = j;
+                }
+                int temp = a[i];
+                a[j] = a[i];
+                a[i] = temp;
+            }
+
+        }
+        return a;
     }
 }
